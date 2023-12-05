e0c30588cdceeaacb50a4509717b2343b6e174e4
Saade <saade@outlook.com.br>
2023-12-05 19:40:13 -0300

    fix: description evaluation parameters

--- src/Components/TimelineEntry/Concerns/HasDescription.php
+++ src/Components/TimelineEntry/Concerns/HasDescription.php
@@ -3,8 +3,16 @@
 namespace Saade\FilamentTimeline\Components\TimelineEntry\Concerns;
 
 use Filament\Support\Concerns\HasDescription as BaseHasDescription;
+use Illuminate\Contracts\Support\Htmlable;
 
 trait HasDescription
 {
     use BaseHasDescription;
+
+    public function getDescription(): string | Htmlable | null
+    {
+        return $this->evaluate($this->description, [
+            'state' => $this->getState(),
+        ]);
+    }
 }
