73412389a5e9157ed6b755d5b038623a2d0cfee6
saade <saade@users.noreply.github.com>
2023-12-05 18:52:00 +0000

    Fix styling

--- src/FilamentTimelineServiceProvider.php
+++ src/FilamentTimelineServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Saade\FilamentTimeline\Commands\FilamentTimelineCommand;
+use Saade\FilamentTimeline\Testing\TestsFilamentTimeline;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Saade\FilamentTimeline\Commands\FilamentTimelineCommand;
-use Saade\FilamentTimeline\Testing\TestsFilamentTimeline;
 
 class FilamentTimelineServiceProvider extends PackageServiceProvider
 {
