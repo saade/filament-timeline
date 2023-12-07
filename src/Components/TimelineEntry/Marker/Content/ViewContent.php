02a1cfa02d3b0c3c0ed6d4fb369b7bc6547f22ae
Saade <saade@outlook.com.br>
2023-12-07 18:29:14 -0300

    feat: add View content

---
+++ src/Components/TimelineEntry/Marker/Content/ViewContent.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Saade\FilamentTimeline\Components\TimelineEntry\Marker\Content;
+
+use Saade\FilamentTimeline\Components\TimelineEntry\Marker\Content;
+
+class ViewContent extends Content
+{
+}
